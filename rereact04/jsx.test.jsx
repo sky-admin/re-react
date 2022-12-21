9a852935c1f5b11c8e7db7be6c430bc1a615e64b
sky-admin <546485299@qq.com>
2022-12-21 20:01:32 +0800

    copy from 03

---
+++ rereact04/jsx.test.jsx
@@ -0,0 +1,77 @@
+import { describe, it, expect } from 'vitest'
+import AReact from './AReact.jsx'
+const act = AReact.act
+
+function sleep(ms) {
+  return new Promise(resolve => {
+    setTimeout(resolve, ms);
+  })
+}
+
+describe('ReReact JSX', () => {
+  it('should render JSX', async () => {
+    const container = document.createElement('div');
+    const element = (
+      <div id="foo">
+        <div id="bar"></div>
+        <button></button>
+      </div>
+    );
+    console.log(element);
+    const root = AReact.createRoot(container);
+    await act(() => {
+      root.render(element);
+    });
+    expect(container.innerHTML).toBe('<div id="foo"><div id="bar"></div><button></button></div>')
+  })
+
+  it('should render JSX with text', async () => {
+    const container = document.createElement('div');
+    const element = (
+      <div id="foo">
+        <div id="bar">Hello</div>
+        <button>Add</button>
+      </div>
+    );
+    console.log(JSON.stringify(element));
+    const root = AReact.createRoot(container);
+    await act(() => {
+      root.render(element);
+    })
+    expect(container.innerHTML).toBe('<div id="foo"><div id="bar">Hello</div><button>Add</button></div>')
+  })
+
+  it('should render JSX with different props', async () => {
+    const container = document.createElement('div');
+    const element = (
+      <div id="foo" className="bar">
+        <button></button>
+      </div>
+    );
+    console.log(element);
+    const root = AReact.createRoot(container);
+    await act(() => {
+      root.render(element);
+    })
+    expect(container.innerHTML).toBe('<div id="foo" class="bar"><button></button></div>')
+  })
+})
+
+describe('ReReact Concurrent', () => {
+  it('should render in async', async function () {
+    const container = document.createElement('div');
+    const element = (
+      <div id="foo">
+        <div id="bar"></div>
+        <button></button>
+      </div>
+    );
+    console.log(element);
+    const root = AReact.createRoot(container);
+    await act(() => {
+      root.render(element);
+      expect(container.innerHTML).toBe('')
+    })
+    expect(container.innerHTML).toBe('<div id="foo"><div id="bar"></div><button></button></div>')
+  })
+})
