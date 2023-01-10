6a7cfb7e98c25e37f4fecb047650bf0c68afdc2a
sky-admin <546485299@qq.com>
2023-01-10 14:09:56 +0800

    feat: create 06 folder copy from 05

---
+++ rereact06/AReact.jsx
@@ -0,0 +1,230 @@
+import '../requestIdleCallbackPolyfill.js';
+
+function createElement(type, props, ...children) {
+  return {
+    type,
+    props: {
+      ...props,
+      children: children.flat().map(child => {
+        return typeof child !== 'object' ? createTextElement(child) : child;
+      })
+    }
+  }
+}
+
+function createTextElement(text) {
+  return {
+    type: 'HostText',
+    props: {
+      nodeValue: text,
+      children: [],
+    }
+  }
+}
+
+const isProperty = (key) => key !== 'children'
+let workInProgress = null;
+let workInProgressRoot = null;
+let currentHookFiber = null;
+let currentHookIndex = 0;
+
+class AReactDomRoot {
+  _internalRoot = null;
+  constructor (container) {
+    this.container = container;
+    this._internalRoot = {
+      current: null,
+      containerInfo: container,
+    }
+  }
+
+  render(element) {
+    this._internalRoot.current = {
+      alternate: {
+        stateNode: this._internalRoot.containerInfo,
+        props: {
+          children: [element]
+        }
+      }
+    }
+    workInProgressRoot = this._internalRoot;
+    workInProgress = workInProgressRoot.current.alternate;
+
+    window.requestIdleCallback(workloop);
+    // setTimeout(workloop);
+    // this.renderImpl(element, this.container);
+  }
+}
+
+function workloop() {
+  while (workInProgress) {
+    workInProgress = performUnitOfWork(workInProgress);
+  }
+
+  if (!workInProgress && workInProgressRoot.current.alternate) {
+    workInProgressRoot.current = workInProgressRoot.current.alternate;
+    workInProgressRoot.current.alternate = null;
+  }
+}
+
+function performUnitOfWork(fiber) {
+  // 处理当前fiber：创建 DOM，设置 props,插入当前dom到parent
+  const isFunctionComponent = fiber.type instanceof Function;
+  if (isFunctionComponent) {
+    // 函数式组件，对hook作记录
+    currentHookFiber = fiber;
+    currentHookFiber.memorizedState = [];
+    currentHookIndex = 0;
+
+    fiber.props.children = [fiber.type(fiber.props)]
+  } else {
+    if (!fiber.stateNode) {
+      fiber.stateNode = fiber.type === 'HostText' ? document.createTextNode('') : document.createElement(fiber.type);
+      Object.keys(fiber.props).filter(isProperty).forEach(key => {
+        fiber.stateNode[key] = fiber.props[key];
+      })
+    }
+    if (fiber.return) {
+      // 往上查找，直到有一个节点存在 stateNode
+      let domParentFiber = fiber.return;
+      while (!domParentFiber.stateNode) {
+        domParentFiber = domParentFiber.return;
+      }
+      domParentFiber.stateNode.appendChild(fiber.stateNode);
+    }
+  }
+
+  // 初始化 children 的 fiber
+  let prevSibling = null;
+  // mount 阶段 oldFiber 为空，update 阶段为上一次的值
+  let oldFiber = fiber.alternate?.child;
+  fiber.props.children.forEach((child, index) => {
+    let newFiber = null;
+    if (!oldFiber) {
+      // mount
+      newFiber = {
+        type: child.type,
+        stateNode: null,
+        props: child.props,
+        return: fiber,
+        alternate: null,
+        child: null,
+        sibling: null,
+      }
+    } else {
+      // update
+      newFiber = {
+        type: child.type,
+        stateNode: oldFiber.stateNode,
+        props: child.props,
+        return: fiber,
+        alternate: oldFiber,
+        child: null,
+        sibling: null,
+      }
+    }
+
+    if (oldFiber) {
+      oldFiber = oldFiber.sibling;
+    }
+
+    if (index === 0) {
+      fiber.child = newFiber;
+    } else {
+      prevSibling.sibling = newFiber;
+    }
+    prevSibling = newFiber;
+  })
+
+  // 返回下一个处理的 fiber
+  return getNextFiber(fiber)
+}
+
+function getNextFiber(fiber)  {
+  // 遍历顺序：
+  // 先遍历 child
+  // 然后是sibling
+  // 然后是return并查找下一个sibling
+  if (fiber.child) {
+    return fiber.child
+  }
+
+  let nextFiber = fiber;
+  while (nextFiber) {
+    if (nextFiber.sibling) {
+      return nextFiber.sibling;
+    } else {
+      nextFiber = nextFiber.return;
+    }
+  }
+  return null;
+}
+
+function createRoot(container) {
+  return new AReactDomRoot(container);
+}
+
+function useState(initialState) {
+  const oldHook = currentHookFiber.alternate?.memorizedState?.[currentHookIndex];
+
+  const hook = {
+    state: oldHook ? oldHook.state : initialState,
+    queue: [],
+    dispatch: oldHook ? oldHook.dispatch : null,
+  }
+
+  const actions = oldHook ? oldHook.queue : [];
+  actions.forEach(action => {
+    hook.state = typeof action === 'function' ? action(hook.state) : action;
+  })
+
+  const setState = oldHook ? oldHook.dispatch : (action) => {
+    hook.queue.push(action);
+
+    // re-render
+    workInProgressRoot.current.alternate = {
+      stateNode: workInProgressRoot.current.containerInfo,
+      props: workInProgressRoot.current.props,
+      alternate: workInProgressRoot.current // 重要，交换 alternate
+    }
+    workInProgress = workInProgressRoot.current.alternate;
+    window.requestIdleCallback(workloop);
+  };
+
+  currentHookFiber.memorizedState.push(hook);
+  currentHookIndex++;
+
+  return [hook.state, setState];
+}
+
+function useReducer(reducer, initialState) {
+  const [state, setState] = useState(initialState);
+  const dispatch = (action) => {
+    // reducer = (oldState, action) => newState
+    setState((state) => reducer(state, action));
+  }
+
+  return [state, dispatch];
+}
+
+function act(callback) {
+  callback();
+  return new Promise((resolve) => {
+    function loop() {
+      if (workInProgress) {
+        window.requestIdleCallback(loop);
+      } else {
+        resolve();
+      }
+    }
+    loop();
+  })
+}
+
+export default {
+  createElement,
+  createRoot,
+  act,
+  useState,
+  useReducer,
+}
---
+++ rereact06/jsx.test.jsx
@@ -0,0 +1,202 @@
+import { describe, it, expect } from 'vitest'
+import AReact from './AReact.jsx'
+const act = AReact.act
+
+function sleep(ms) {
+  return new Promise(resolve => {
+    setTimeout(resolve, ms);
+  })
+}
+
+describe('ReReact JSX', () => {
+  it('should render JSX', async () => {
+    const container = document.createElement('div');
+    const element = (
+      <div id="foo">
+        <div id="bar"></div>
+        <button></button>
+      </div>
+    );
+    console.log(element);
+    const root = AReact.createRoot(container);
+    await act(() => {
+      root.render(element);
+    });
+    expect(container.innerHTML).toBe('<div id="foo"><div id="bar"></div><button></button></div>')
+  })
+
+  it('should render JSX with text', async () => {
+    const container = document.createElement('div');
+    const element = (
+      <div id="foo">
+        <div id="bar">Hello</div>
+        <button>Add</button>
+      </div>
+    );
+    console.log(JSON.stringify(element));
+    const root = AReact.createRoot(container);
+    await act(() => {
+      root.render(element);
+    })
+    expect(container.innerHTML).toBe('<div id="foo"><div id="bar">Hello</div><button>Add</button></div>')
+  })
+
+  it('should render JSX with different props', async () => {
+    const container = document.createElement('div');
+    const element = (
+      <div id="foo" className="bar">
+        <button></button>
+      </div>
+    );
+    console.log(element);
+    const root = AReact.createRoot(container);
+    await act(() => {
+      root.render(element);
+    })
+    expect(container.innerHTML).toBe('<div id="foo" class="bar"><button></button></div>')
+  })
+})
+
+describe('ReReact Concurrent', () => {
+  it('should render in async', async function () {
+    const container = document.createElement('div');
+    const element = (
+      <div id="foo">
+        <div id="bar"></div>
+        <button></button>
+      </div>
+    );
+    console.log(element);
+    const root = AReact.createRoot(container);
+    await act(() => {
+      root.render(element);
+      expect(container.innerHTML).toBe('')
+    })
+    expect(container.innerHTML).toBe('<div id="foo"><div id="bar"></div><button></button></div>')
+  })
+})
+
+describe('Function Component', () => {
+  it('should render Function Component', async function () {
+    const container = document.createElement('div');
+    function App() {
+      return (
+        <div id="foo">
+          <div id="bar"></div>
+          <button></button>
+        </div>
+      );
+    }
+    const root = AReact.createRoot(container);
+    await act(() => {
+      root.render(<App />);
+      expect(container.innerHTML).toBe('')
+    })
+    expect(container.innerHTML).toBe('<div id="foo"><div id="bar"></div><button></button></div>')
+  })
+
+  it('should render nested Function Component', async function () {
+    const container = document.createElement('div');
+    function App(props) {
+      return (
+        <div id="foo">
+          <div id="bar">{props.title}</div>
+          <button></button>
+          {props.children}
+        </div>
+      );
+    }
+    const root = AReact.createRoot(container);
+    await act(() => {
+      root.render(
+        <App title='main title'>
+          <App title='sub title'/>
+        </App>
+      );
+      expect(container.innerHTML).toBe('')
+    })
+    expect(container.innerHTML).toBe('<div id="foo"><div id="bar">main title</div><button></button><div id="foo"><div id="bar">sub title</div><button></button></div></div>')
+  })
+})
+
+describe('Hooks', () => {
+  it('should support useState', async function () {
+    const container = document.createElement('div');
+    const globalObject = {};
+
+    function App() {
+      const [count, setCount] = AReact.useState(100);
+      globalObject.count = count;
+      globalObject.setCount = setCount;
+
+      return (<div>{count}</div>)
+    }
+
+    const root = AReact.createRoot(container);
+    await act(() => {
+      root.render(<App/>);
+    });
+
+    await act(() => {
+      globalObject.setCount((count) => count + 1);
+    });
+
+    expect(globalObject.count).toBe(101)
+  })
+
+  it('should support useState pass value', async function () {
+    const container = document.createElement('div');
+    const globalObject = {};
+
+    function App() {
+      const [count, setCount] = AReact.useState(100);
+      globalObject.count = count;
+      globalObject.setCount = setCount;
+
+      return (<div>{count}</div>)
+    }
+
+    const root = AReact.createRoot(container);
+    await act(() => {
+      root.render(<App/>);
+    });
+
+    await act(() => {
+      globalObject.setCount(102);
+    });
+
+    expect(globalObject.count).toBe(102)
+  })
+
+  it('should support useReducer', async function () {
+    const container = document.createElement('div');
+    const globalObject = {};
+
+    function reducer(state, action) {
+      switch (action.type) {
+        case 'add': return state + 1;
+        case 'sub': return state - 1;
+      }
+    }
+
+    function App() {
+      const [count, dispatch] = AReact.useReducer(reducer, 100);
+      globalObject.count = count;
+      globalObject.dispatch = dispatch;
+
+      return (<div>{count}</div>)
+    }
+
+    const root = AReact.createRoot(container);
+    await act(() => {
+      root.render(<App/>);
+    });
+
+    await act(() => {
+      globalObject.dispatch({ type: 'add' });
+      globalObject.dispatch({ type: 'add' });
+    });
+
+    expect(globalObject.count).toBe(102)
+  })
+})
